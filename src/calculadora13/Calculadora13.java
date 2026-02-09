cb35be8ae7e6b0bf6e2681a7f554d5ba2a443c34
Beatriz Santos <beatriz.santos03@somo.eus>
2026-02-09 19:21:42 +0100

    Añadidos comentarios de Javadoc

--- src/calculadora13/Calculadora13.java
+++ src/calculadora13/Calculadora13.java
@@ -1,23 +1,52 @@
 package calculadora13;
+
 /**
- * Clase CalculadoraGIT13
- * Calculadora básica del ejercicio 6.
- @author Beatriz
- */
+ * Clase CalculadoraGIT13 Calculadora básica del ejercicio 6.
+ * 
+ * @author Beatriz
+ */	
+	/**
+	 * Suma dos números enteros.
+	 *
+	 * @param a primer operando
+	 * @param b segundo operando
+	 * @return resultado de la suma
+	 */
 public class Calculadora13 {
 
 	public int sumar(int a, int b) {
 		return a + b;
 	}
 
+	/**
+	 * restar dos números enteros.
+	 *
+	 * @param a primer operando
+	 * @param b segundo operando
+	 * @return resultado de la resta
+	 */
 	public int restar(int a, int b) {
 		return a - b;
 	}
 
+	/**
+	 * multiplicar dos números enteros.
+	 *
+	 * @param a primer operando
+	 * @param b segundo operando
+	 * @return resultado de la multiplicacion
+	 */
 	public int multiplicar(int a, int b) {
 		return a * b;
 	}
 
+	/**
+	 * dividir dos números enteros.
+	 *
+	 * @param a primer operando
+	 * @param b segundo operando
+	 * @return resultado de la division
+	 */
 	public double dividir(int a, int b) {
 		if (b == 0) {
 			System.out.println("Error: división entre cero");
@@ -25,6 +54,14 @@ public class Calculadora13 {
 		}
 		return (double) a / b;
 	}
+
+	/**
+	 * potencia de dos números enteros.
+	 *
+	 * @param a primer operando
+	 * @param b segundo operando
+	 * @return resultado de la potencia
+	 */
 
 	public int potencia(int base, int exponente) {
 		int resultado = 1;
