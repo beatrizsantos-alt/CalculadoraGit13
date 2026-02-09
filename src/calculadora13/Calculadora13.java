dddefa7f155cd4221a4b42cf49c528422471af0d
Beatriz Santos <beatriz.santos03@somo.eus>
2026-02-09 10:32:38 +0100

    Modificado el metodo division en caso de que el divisor sea 0

--- src/calculadora13/Calculadora13.java
+++ src/calculadora13/Calculadora13.java
@@ -9,4 +9,15 @@ public class Calculadora13 {
 	public int restar(int a, int b) {
 		return a - b;
 	}
+
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+
+	public double dividir(int a, int b) {
+		if (b == 0) {
+			throw new IllegalArgumentException("No se puede dividir entre cero");
+		}
+		return (double) a / b;
+	}
 }
